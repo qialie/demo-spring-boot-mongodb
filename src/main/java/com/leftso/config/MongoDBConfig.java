f20901bbae57689b27b9e78a9f05f23c7f74205c
leftso <leftso@qq.com>
2017-03-29 14:44:38 +0800

    数据库配置移除代码,使用属性文件

--- src/main/java/com/leftso/config/MongoDBConfig.java
+++ src/main/java/com/leftso/config/MongoDBConfig.java
@@ -1,10 +1,12 @@
 package com.leftso.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
+import org.springframework.util.StringUtils;
 
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
@@ -17,6 +19,8 @@ import com.mongodb.MongoClientURI;
  */
 @Configuration
 public class MongoDBConfig {
+	@Autowired
+	MongoProperty mongoProperties;
 
 	/**
 	 * 注入mongodb的工厂类
@@ -26,16 +30,24 @@ public class MongoDBConfig {
 	@Bean
 	public MongoDbFactory mongoDbFactory() {
 		// uri格式mongodb://[username:password@]host1[:port1][,host2[:port2],...[,hostN[:portN]]][/[database][?options]]
-		String mongoURI = "mongodb://localhost:27017";// 为了方便实现mongodb多数据库和数据库的负债均衡这里使用url方式创建工厂
-		String databaseName = "test";
+		String mongoURI = "mongodb://" + mongoProperties.getHost();
+		if (!StringUtils.isEmpty(mongoProperties.getUser())) {
+			mongoURI = "mongodb://" + mongoProperties.getUser() + ":" + mongoProperties.getPwd() + "@"
+					+ mongoProperties.getHost();
+		}
+		// 为了方便实现mongodb多数据库和数据库的负债均衡这里使用url方式创建工厂
 		MongoClientURI mongoClientURI = new MongoClientURI(mongoURI);
 		MongoClient mongoClient = new MongoClient(mongoClientURI);
-		MongoDbFactory mongoDbFactory = new SimpleMongoDbFactory(mongoClient, databaseName);
-		//注意:以下构造函数已经弃用:
-		//SimpleMongoDbFactory(com.mongodb.Mongo mongo, String databaseName); 弃用版本1.7
-		//SimpleMongoDbFactory(com.mongodb.Mongo mongo, String databaseName, UserCredentials credentials);弃用版本1.7
-		//SimpleMongoDbFactory(com.mongodb.Mongo mongo, String databaseName, UserCredentials credentials, String authenticationDatabaseName);弃用版本1.7
-		//SimpleMongoDbFactory(com.mongodb.MongoURI uri);弃用版本1.7
+		MongoDbFactory mongoDbFactory = new SimpleMongoDbFactory(mongoClient, mongoProperties.getName());
+		// 注意:以下构造函数已经弃用:
+		// SimpleMongoDbFactory(com.mongodb.Mongo mongo, String databaseName);
+		// 弃用版本1.7
+		// SimpleMongoDbFactory(com.mongodb.Mongo mongo, String databaseName,
+		// UserCredentials credentials);弃用版本1.7
+		// SimpleMongoDbFactory(com.mongodb.Mongo mongo, String databaseName,
+		// UserCredentials credentials, String
+		// authenticationDatabaseName);弃用版本1.7
+		// SimpleMongoDbFactory(com.mongodb.MongoURI uri);弃用版本1.7
 		return mongoDbFactory;
 	}
 
